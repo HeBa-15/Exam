5431aec1feb78890ce201a849dcb65f15e08550a
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-14 21:19:20 +0200

    Class Question &Answer

---
+++ Exam/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Answer
+    {
+        public int answerId { get; set; }
+        public string answer { get; set; }
+
+        public Answer(int answerId, string answer)
+        {
+            this.answerId = answerId;
+            this.answer = answer;
+        }
+
+    }
+}
---
+++ Exam/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+        public Answer[] answerList { get; set; }
+        public Answer RightAnswer { get; set; }
+
+
+        public Question(string Header, string Body, int Mark)
+        {
+            Header = Header;
+            Body = Body;
+            Mark = Mark;
+           
+
+        }
+        public virtual void display()
+        {
+
+            Console.WriteLine(Header + "\t\t " + "Mark : " + Mark);
+            Console.WriteLine(Body);
+
+
+        }
+    }
+}
